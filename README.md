3b3f5f6bb8e749698d77a3572f9c221ef2fa3a19
Calvin Nhieu <calvin.nhieu@gmail.com>
2020-12-15 17:14:03 -0500

    Create React page template

--- README.md
+++
@@ -1,3 +0,0 @@
-My personal website
-
-02.15.17
---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div style={{width: '100%', height: '100%', display: 'flex'}}>
+        <div style={{width: '50%'}}/>
+        <div style={{width: '50%', background: '#996060'}}/>
+      </div>
+    );
+  }
+}
