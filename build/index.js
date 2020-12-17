1f607ce20bb2da0d6cec6eddfba570e91cbaec44
Calvin Nhieu <calvin.nhieu@gmail.com>
2020-12-16 21:16:20 -0500

    Create loading animations Create hover animations Set monochrome theme

--- build/index.js
+++ build/index.js
@@ -8,6 +8,14 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var OPACITY = 0.9;
+var MENU_COLOURS = {
+  RED: 'rgba(255, 215, 215, ' + OPACITY + ')',
+  YELLOW: 'rgba(255, 241, 205, ' + OPACITY + ')',
+  BLUE: 'rgba(218, 228, 255, ' + OPACITY + ')',
+  GREEN: 'rgba(222, 255, 222, ' + OPACITY + ')'
+};
+
 var Title = function (_React$Component) {
   _inherits(Title, _React$Component);
 
@@ -22,11 +30,20 @@ var Title = function (_React$Component) {
     value: function render() {
       return React.createElement(
         'div',
-        { style: { display: 'flex', flexDirection: 'column' } },
+        { style: { display: 'flex', flexDirection: 'column', cursor: this.props.isHover ? 'context-menu' : 'default' } },
         React.createElement(
           'h1',
-          { style: this.props.isHover ? { textDecoration: 'underline rgba(221,221,221,0.3)', textDecorationThickness: '7px', cursor: 'context-menu' } : {} },
-          'Calvin Nhieu'
+          null,
+          React.createElement(
+            'span',
+            { className: 'name-fade-in' },
+            'Calvin '
+          ),
+          React.createElement(
+            'span',
+            { className: 'name-fade-in name-delay' },
+            'Nhieu'
+          )
         )
       );
     }
@@ -35,13 +52,58 @@ var Title = function (_React$Component) {
   return Title;
 }(React.Component);
 
-var Menu = function (_React$Component2) {
-  _inherits(Menu, _React$Component2);
+var MenuItem = function (_React$Component2) {
+  _inherits(MenuItem, _React$Component2);
 
-  function Menu(props) {
+  function MenuItem(props) {
+    _classCallCheck(this, MenuItem);
+
+    var _this2 = _possibleConstructorReturn(this, (MenuItem.__proto__ || Object.getPrototypeOf(MenuItem)).call(this, props));
+
+    _this2.state = { isHover: false };
+    return _this2;
+  }
+
+  _createClass(MenuItem, [{
+    key: 'render',
+    value: function render() {
+      var _this3 = this;
+
+      return React.createElement(
+        'div',
+        {
+          style: { display: 'flex', flexDirection: 'column', marginBottom: '20px', position: 'relative', animation: 'text-fade-in 0.9s ease-out ' + (1 + this.props.idx * 0.3) + 's both' },
+          onMouseEnter: function onMouseEnter() {
+            return _this3.setState({ isHover: true });
+          },
+          onMouseLeave: function onMouseLeave() {
+            return _this3.setState({ isHover: false });
+          }
+        },
+        React.createElement(
+          'p',
+          null,
+          React.createElement(
+            'a',
+            { href: this.props.link, target: '_blank' },
+            this.props.text
+          )
+        ),
+        React.createElement('div', { className: this.state.isHover ? 'hover-menu-item' : '', style: { visibility: this.state.isHover ? 'visible' : 'hidden', width: '45px', height: '9px', background: this.props.colour, position: 'absolute', top: '18px' } })
+      );
+    }
+  }]);
+
+  return MenuItem;
+}(React.Component);
+
+var Menu = function (_React$Component3) {
+  _inherits(Menu, _React$Component3);
+
+  function Menu() {
     _classCallCheck(this, Menu);
 
-    return _possibleConstructorReturn(this, (Menu.__proto__ || Object.getPrototypeOf(Menu)).call(this, props));
+    return _possibleConstructorReturn(this, (Menu.__proto__ || Object.getPrototypeOf(Menu)).apply(this, arguments));
   }
 
   _createClass(Menu, [{
@@ -50,42 +112,10 @@ var Menu = function (_React$Component2) {
       return React.createElement(
         'div',
         { style: { visibility: this.props.show ? 'visible' : 'hidden', display: 'absolute' } },
-        React.createElement(
-          'p',
-          null,
-          React.createElement(
-            'a',
-            { href: '', target: '_blank' },
-            'About'
-          )
-        ),
-        React.createElement(
-          'p',
-          null,
-          React.createElement(
-            'a',
-            { href: '', target: '_blank' },
-            'Projects'
-          )
-        ),
-        React.createElement(
-          'p',
-          null,
-          React.createElement(
-            'a',
-            { href: 'https://github.com/calvinnhieu', target: '_blank' },
-            'GitHub'
-          )
-        ),
-        React.createElement(
-          'p',
-          null,
-          React.createElement(
-            'a',
-            { href: '', target: '_blank' },
-            'Resume'
-          )
-        )
+        React.createElement(MenuItem, { text: 'About', link: '', colour: MENU_COLOURS.RED, idx: 0 }),
+        React.createElement(MenuItem, { text: 'Projects', link: '', colour: MENU_COLOURS.YELLOW, idx: 1 }),
+        React.createElement(MenuItem, { text: 'Github', link: 'https://github.com/calvinnhieu', colour: MENU_COLOURS.BLUE, idx: 2 }),
+        React.createElement(MenuItem, { text: 'Resume', link: '', colour: MENU_COLOURS.GREEN, idx: 3 })
       );
     }
   }]);
@@ -93,25 +123,25 @@ var Menu = function (_React$Component2) {
   return Menu;
 }(React.Component);
 
-var Main = function (_React$Component3) {
-  _inherits(Main, _React$Component3);
+var Main = function (_React$Component4) {
+  _inherits(Main, _React$Component4);
 
   function Main(props) {
     _classCallCheck(this, Main);
 
-    var _this3 = _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).call(this, props));
+    var _this5 = _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).call(this, props));
 
-    _this3.state = {
-      showMenu: false,
+    _this5.state = {
+      showMenu: true,
       isHoverTitle: false
     };
-    return _this3;
+    return _this5;
   }
 
   _createClass(Main, [{
     key: 'render',
     value: function render() {
-      var _this4 = this;
+      var _this6 = this;
 
       return React.createElement(
         'div',
@@ -123,13 +153,13 @@ var Main = function (_React$Component3) {
             'div',
             {
               onClick: function onClick() {
-                return _this4.setState({ showMenu: !_this4.state.showMenu });
+                return _this6.setState({ showMenu: !_this6.state.showMenu });
               },
               onMouseEnter: function onMouseEnter() {
-                return _this4.setState({ isHoverTitle: true });
+                return _this6.setState({ isHoverTitle: true });
               },
               onMouseLeave: function onMouseLeave() {
-                return _this4.setState({ isHoverTitle: false });
+                return _this6.setState({ isHoverTitle: false });
               }
             },
             React.createElement(Title, { isHover: this.state.isHoverTitle })
--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
 'use strict';
 
+const OPACITY = 0.9
+const MENU_COLOURS = {
+  RED: `rgba(255, 215, 215, ${OPACITY})`,
+  YELLOW: `rgba(255, 241, 205, ${OPACITY})`,
+  BLUE: `rgba(218, 228, 255, ${OPACITY})`,
+  GREEN: `rgba(222, 255, 222, ${OPACITY})`
+}
+
 class Title extends React.Component {
   render() {
     return (
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <h1 style={this.props.isHover ? {textDecoration: 'underline rgba(221,221,221,0.3)', textDecorationThickness: '7px', cursor: 'context-menu'} : {}}>Calvin Nhieu</h1>
+      <div style={{display: 'flex', flexDirection: 'column', cursor: this.props.isHover ? 'context-menu' : 'default'}}>
+        <h1><span className="name-fade-in">Calvin </span><span className="name-fade-in name-delay">Nhieu</span></h1>
       </div>
     );
   }
 }
 
-class Menu extends React.Component {
+class MenuItem extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {isHover: false}
   }
 
   render() {
     return (
+      <div 
+        style={{display: 'flex', flexDirection: 'column', marginBottom: '20px', position: 'relative', animation: `text-fade-in 0.9s ease-out ${1 + (this.props.idx * 0.3)}s both`}}
+        onMouseEnter={() => this.setState({isHover: true})}
+        onMouseLeave={() => this.setState({isHover: false})}
+      >
+        <p><a href={this.props.link} target="_blank">{this.props.text}</a></p>
+        <div className={this.state.isHover ? 'hover-menu-item' : ''}style={{visibility: this.state.isHover ? 'visible' : 'hidden', width: '45px', height: '9px', background: this.props.colour, position: 'absolute', top: '18px'}}/>
+      </div>
+    )
+  }
+}
+class Menu extends React.Component {
+  render() {
+    return (
       <div style={{visibility: this.props.show ? 'visible' : 'hidden', display: 'absolute'}}>
-        <p><a href="" target="_blank">About</a></p>
-        <p><a href="" target="_blank">Projects</a></p>
-        <p><a href="https://github.com/calvinnhieu" target="_blank">GitHub</a></p>
-        <p><a href="" target="_blank">Resume</a></p>
+	<MenuItem text="About" link="" colour={MENU_COLOURS.RED} idx={0}/>
+	<MenuItem text="Projects" link="" colour={MENU_COLOURS.YELLOW} idx={1}/>
+	<MenuItem text="Github" link="https://github.com/calvinnhieu" colour={MENU_COLOURS.BLUE} idx={2}/>
+	<MenuItem text="Resume" link="" colour={MENU_COLOURS.GREEN} idx={3}/>
       </div>
     );
   }
@@ -31,7 +54,7 @@ class Main extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      showMenu: false,
+      showMenu: true,
       isHoverTitle: false
     }
   }
